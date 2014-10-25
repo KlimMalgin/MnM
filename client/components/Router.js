e3113818d2f3aeff6a7ba64e32490e1744f5c8bf
KlimMalgin <fxklim@gmail.com>
2014-10-25 14:57:25 +0500

    Рефакторинг роутера

--- client/components/Router.js
+++ client/components/Router.js
@@ -5,7 +5,11 @@
 
 
 var React = require('react'),
-    Router = require('react-router');
+    Router = require('react-router'),
+    Routes = Router.Routes,
+    Route = Router.Route,
+    DefaultRoute = Router.DefaultRoute,
+    NotFoundRoute = Router.NotFoundRoute;
 
 var Application = require('./Application'),
     Form = require('./Form/Form');
