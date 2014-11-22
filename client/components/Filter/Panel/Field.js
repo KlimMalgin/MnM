96e36e74f98c90a877428832568d533812ca0756
KlimMalgin <fxklim@gmail.com>
2014-11-22 22:41:41 +0500

    valueLink для фильтрующего поля. Native parse API

--- client/components/Filter/Panel/Field.js
+++ client/components/Filter/Panel/Field.js
@@ -11,10 +11,32 @@ var Bootstrap = require('react-bootstrap'),
 
 var Field = React.createClass({
 
+    getInitialState: function () {
+        return {
+            value: ''
+        };
+    },
+
+    setValue: function (v) {
+        /*TransactionActions.loadTransactions({
+
+        });*/
+        this.setState({
+            value: v
+        });
+    },
+
     render: function () {
+        var fieldLink = {
+            value: this.state.value,
+            requestChange: this.setValue
+        };
+
+        console.log(this.state.value);
+
         return (
             <div className="search-field">
-                <Input type="text" placeholder="filter..." />
+                <Input type="text" placeholder="filter..." valueLink={fieldLink} />
             </div>
         );
     }
