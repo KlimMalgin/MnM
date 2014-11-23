103f051472509d2ead2e3d429288122989ecc41a
KlimMalgin <fxklim@gmail.com>
2014-11-23 11:34:38 +0500

    Рендер тегов

--- client/components/Filter/Tile/Tags.js
+++ client/components/Filter/Tile/Tags.js
@@ -8,9 +8,23 @@ var React = require('react');
 
 var Tags = React.createClass({
 
+    getDefaultProps: function () {
+        return {
+            children: []
+        };
+    },
+
+    renderTagItem: function (item) {
+        return <span className="tag-item">[{item.name}]</span>;
+    },
+
+    renderTags: function () {
+        return this.props.children.map(this.renderTagItem);
+    },
+
     render: function () {
         return (
-            <div className="tags">[tags]</div>
+            <div className="tags">{this.renderTags()}</div>
         );
     }
 
