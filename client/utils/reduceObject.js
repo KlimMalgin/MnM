1afdd8e2f037c5b838356645445f403371bbd06c
KlimMalgin <fxklim@gmail.com>
2014-12-20 14:31:39 +0500

    Дополнил утилиту reduceObject

--- client/utils/reduceObject.js
+++ client/utils/reduceObject.js
@@ -2,11 +2,27 @@
  * Created by laiff on 30.10.14.
  */
 
-module.exports = function(object, reducer) {
+var merge = require('react/lib/merge');
+
+function objectReducer (object, reducer) {
+    var acc = {};
+    for (var property in object) {
+        if (object.hasOwnProperty(property))
+            acc = merge(acc, reducer(object[property]));
+    }
+    return acc;
+}
+
+function objectToArrayReducer (object, reducer) {
     var acc = [];
     for (var property in object) {
         if (object.hasOwnProperty(property))
             acc = acc.concat([reducer(object[property])]);
     }
     return acc.length === 0 ? null : acc;
+}
+
+module.exports = {
+    objectReducer: objectReducer,
+    objectToArray: objectToArrayReducer
 };
