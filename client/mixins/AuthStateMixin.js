5ce3848af3c6da6f2b576a08ec657c941e2d25e1
KlimMalgin <fxklim@gmail.com>
2014-12-21 00:40:33 +0500

    Mixin для проверки авторизации

---
+++ client/mixins/AuthStateMixin.js
@@ -0,0 +1,24 @@
+/**
+ * Created by KlimMalgin on 21.12.2014.
+ */
+'use strict';
+
+var Reflux = require('reflux'),
+    ListenerMixin = Reflux.ListenerMixin;
+
+var UserStore = require('../stores/User/Store');
+
+var AuthStateMixin = {
+
+    mixins: [
+        ListenerMixin,
+        Reflux.connect(UserStore, 'user')
+    ],
+
+    isAuth: function () {
+        return this.state.user
+    }
+
+};
+
+module.exports = {};
