120a283a3126929d7e8b894b994f525484f15609
KlimMalgin <fxklim@gmail.com>
2014-12-25 10:40:40 +0500

    Cloud Function для выборки из тегов текущего юзера по частичному совпадению с фразой

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -4,3 +4,22 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("getTagsByPhrase", function(request, response) {
+  var query = new Parse.Query("Tags");
+  query.equalTo("userId", request.params.userId);
+  query.find({
+    success: function (results) {
+      var result = [];
+      for (var i = 0; i < results.length; ++i) {
+        if (results[i].get("text").indexOf(request.params.phrase) >= 0) {
+          result.push(results[i]);
+        }
+      }
+      response.success(result);
+    },
+    error: function () {
+      response.error("request tags failed!");
+    }
+  });
+});
